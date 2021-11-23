af25755d5decb992bf18c6259ec581624b1aeb3f
Phil Gee <philgeejunk@gmail.com>
2021-11-22 23:31:06 -0500

    added CSS and refactored a bit

--- main.js
+++ main.js
@@ -2,44 +2,66 @@
 let runProgram, userName, num1, num2, sum;
 
 // Click event triggering welcome prompt and addTwoNumbers function
-document.getElementById('initiate-button').addEventListener('click', () => {
+document.getElementById('initiate-button').addEventListener('click', () => mainProgram());
+
+const mainProgram = () => {
     if (!welcomePrompt()) return; // exit if the user clicks "Cancel"
     do {
-        if (!addTwoNumbers()) return; // exit if the user clicks "Cancel"
+        if (!calculatePrompt()) return; // exit if the user clicks "Cancel"
+        displayLastCalculation(num1, num2, sum, userName)
         runProgram = confirm('Add another set of numbers?')
     } while (runProgram)
-});
+}
 
-// Arrow function that handles the welcome prompt for your name
-let welcomePrompt = () => {
-    alert('Hello!')
+// function that handles the welcome prompt for your name
+const welcomePrompt = () => {
+    alert('Welcome to Phil\'s amazing calculator!')
+
     do {
         userName = prompt('What is your name?')
         if (userName === null) return false;
     }
-    while (userName == '')
-    alert('Welcome, ' + userName)
+    while (userName == '') // keep asking if blank
+
+    alert(`Welcome, ${userName}!`)
+
     return true;
 };
 
-let addTwoNumbers = () => {
+const calculatePrompt = () => {
+
     do {
         num1 = prompt('What is the first number?')
         if (num1 === null) return false;
     }
-    while (isNaN(num1) || num1 == '')
+    while (isNaN(num1) || num1 == '') // keep asking if not a number or blank
+
     do {
         num2 = prompt('What is the second number?')
         if (num2 === null) return false;
     }
-    while (isNaN(num2) || num2 == '')
-    sum = parseInt(num1) + parseInt(num2)
-    alert('The sum of your two numbers is: ' + sum)
+    while (isNaN(num2) || num2 == '') // keep asking if not a number or blank
+
+    sum = addNumbers(parseInt(num1), parseInt(num2))
+
+    alert(`The sum of your two numbers is: ${sum}`)
+
     if (sum > 10) {
         alert('That is a big number!')
     } else {
         alert('That is a small number.')
     }
+
     return true;
 };
 
+const addNumbers = (a, b) => {
+    return a + b;
+}
+
+const displayLastCalculation = (num1, num2, sum, userName) => {
+    document.getElementById('num1').innerText = num1.toString()
+    document.getElementById('num2').innerText = num2.toString()
+    document.getElementById('sum').innerText = sum.toString()
+    document.getElementById('userName').innerText = userName.toString()
+}
